f7aa42a20fccc71a5193b02759f0e9ffa9da3ea1
saranga thathsara <saranga.2019952@iit.ac.lk>
2020-11-21 04:57:36 +0530

    version beta
    have to improe with some validations

--- src/com/company/Components.java
+++ src/com/company/Components.java
@@ -23,23 +23,9 @@ public class Components {
         btn.setLayoutY(y);
         btn.setPrefHeight(prefHeight);
         btn.setPrefWidth(setPrefWidth);
-        btn.setStyle("-fx-background-color:#000000; -fx-border-color:#F0F8FF;-fx-text-fill:#f5f7f7;-fx-font-size:2em;-fx-border-radius:20");
+        btn.setStyle("-fx-background-color:transparent;-fx-border-color:#66CDAA;-fx-text-fill:#f5f7f7;-fx-font-size:2em;-fx-border-radius:20;");
         return btn;
     }
-
-    //method for creating labels
-    public static Label creatingLabel(String txtOnLbl, Double x, Double y, Double prefHeight, Double PrefWidth) {
-        Label lbl = new Label(txtOnLbl);
-        lbl.setLayoutX(x);
-        lbl.setLayoutY(y);
-        lbl.setPrefHeight(prefHeight);
-        lbl.setPrefWidth(PrefWidth);
-        lbl.setStyle("-fx-text-fill:#ffffff");
-        lbl.setStyle("-fx-background-color:transparent;-fx-text-fill:#f5f7f7;-fx-font-size:1em;-fx-font-weight: Bold;");
-        return lbl;
-
-    }
-
     //creating label for get dark appearence in all windows
     public static Label creatingLabelForBackground() {
         Label lblBG = new Label();
@@ -48,94 +34,19 @@ public class Components {
         lblBG.setPrefHeight(500);
         lblBG.setPrefWidth(700);
         lblBG.setStyle("-fx-background-color: #000000;");
-        lblBG.setOpacity(0.81);
+        lblBG.setOpacity(0.7);
         return lblBG;
     }
-
     //method for creating text fields
-    public static TextField creatingTextField(String promptTxt, int x, int y, int prefHeight, int PrefWidth) {
+    public static TextField creatingTextField(int x, int y, int prefHeight, int PrefWidth) {
         TextField txtField = new TextField();
-        txtField.setPromptText(promptTxt);
         txtField.setLayoutX(x);
         txtField.setLayoutY(y);
         txtField.setPrefWidth(PrefWidth);
         txtField.setPrefHeight(prefHeight);
-        txtField.setStyle("-fx-background-color:#000000; -fx-border-color:#F0F8FF;-fx-text-fill:#f5f7f7;-fx-font-size:2em;-fx-border-radius:20");
+        txtField.setStyle("-fx-background-color:transparent;-fx-border-color:#66CDAA;-fx-text-fill:#f5f7f7;-fx-font-size:2em;-fx-border-radius:20;");
         return txtField;
     }
-
-    //method for create back button, back button appears in most of the windows and this will avoid code duplication
-    public static Button createBtnBack() {
-        Image imageBack = new Image("Pics/back.png");
-        ImageView backPng = new ImageView();
-        backPng.setImage(imageBack);
-        backPng.setFitWidth(30);
-        backPng.setFitHeight(30);
-
-        Button btnBack = new Button();
-        btnBack.setLayoutX(0);
-        btnBack.setLayoutY(0);
-        btnBack.setPrefHeight(20);
-        btnBack.setPrefWidth(30);
-        btnBack.setGraphic(backPng);
-        btnBack.setStyle("-fx-background-color:transparent; -fx-background-radius:100;");
-        btnBack.setOpacity(0.81);
-        return btnBack;
-    }
-
-    //method for create close button, close button appears in all windows and this will avoid code duplication
-    public static Button createBtnClose() {
-        Image imageClose = new Image("Pics/Close.png");
-        ImageView closePng = new ImageView();
-        closePng.setImage(imageClose);
-        closePng.setFitWidth(30);
-        closePng.setFitHeight(29);
-
-        Button btnClose = new Button();
-        btnClose.setLayoutX(560);
-        btnClose.setLayoutY(0);
-        btnClose.setPrefHeight(19);
-        btnClose.setPrefWidth(30);
-        btnClose.setGraphic(closePng);
-        btnClose.setStyle("-fx-background-color:transparent; -fx-background-radius:100;");
-        btnClose.setOpacity(0.81);
-        return btnClose;
-    }
-
-    //method for creating help button
-    public static Button createBtnHelp() {
-        Image imageHelp = new Image("Pics/help.png");
-        ImageView helpPng = new ImageView();
-        helpPng.setImage(imageHelp);
-        helpPng.setFitWidth(40);
-        helpPng.setFitHeight(40);
-
-        Button btnClose = new Button();
-        btnClose.setLayoutX(2);
-        btnClose.setLayoutY(350);
-        btnClose.setPrefHeight(19);
-        btnClose.setPrefWidth(30);
-        btnClose.setGraphic(helpPng);
-        btnClose.setStyle("-fx-background-color:transparent; -fx-background-radius:100;");
-        btnClose.setOpacity(0.81);
-        return btnClose;
-    }
-
-    //method for keyboard keys, all keyboard keys wii created in this method
-    public static Button btnForKeyBoard(String txtOnBtn, Double x, Double y, Double prefHeight, Double setPrefWidth) {
-        Button key = new Button(txtOnBtn);
-        key.setLayoutX(x);
-        key.setLayoutY(y);
-        key.setPrefHeight(prefHeight);
-        key.setPrefWidth(setPrefWidth);
-        key.setStyle("-fx-background-color:transparent;-fx-border-color:#66CDAA;-fx-text-fill:#f5f7f7;-fx-font-size:2em;-fx-border-radius:20;");
-        return key;
-
-    }
-
-    //method for keyboard, this method can be called with (x,y) values
-
-
     //method for information alert
     public static Alert information(String message) {
         Alert infoAlert = new Alert(Alert.AlertType.NONE);
@@ -144,23 +55,12 @@ public class Components {
         infoAlert.showAndWait();
         return infoAlert;
     }
-
     //method for error alert and this will pop up with a sound
     public static Alert error(String message) {
         Toolkit.getDefaultToolkit().beep();
         Alert errorAlert = new Alert(Alert.AlertType.NONE);
         errorAlert.setAlertType(Alert.AlertType.ERROR);
         errorAlert.setContentText(message);
-        errorAlert.showAndWait();
-        return errorAlert;
-    }
-
-    //if input values are less than 0 this will popup with a sound
-    public static Alert errorInput() {
-        Toolkit.getDefaultToolkit().beep();
-        Alert errorAlert = new Alert(Alert.AlertType.NONE);
-        errorAlert.setAlertType(Alert.AlertType.ERROR);
-        errorAlert.setContentText("Please make sure not to  enter (-) values as inputs");
         errorAlert.showAndWait();
         return errorAlert;
     }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,33 +5,46 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     public static Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
-    public static Button btnAdd, btnMinus, btnMulti, btnDivide, btnEqual, btnPoint;
-    public static TextField calTxtField, qField;
+    public static Button btnAdd, btnMinus, btnMulti, btnDivide, btnEqual, btnPoint, btnClearEntry, btnClear;
+    public static TextField calTxtField, memoryField;
+    public static Label lblBG;
     int operation = -1;
     Float data = 0f;
-    Button[] calButtons = {btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btnAdd, btnMinus, btnMulti, btnDivide, btnEqual, btnPoint};
-    String[] textOnBtn = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "-", "*", "/", "=", "."};
+    Button[] calButtons = {btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btnAdd, btnMinus, btnMulti, btnDivide, btnEqual, btnPoint, btnClearEntry, btnClear};
+    String[] textOnBtn = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "-", "*", "/", "=", ".", "CE", "C"};
     Integer[][] point = {{80, 402}, {0, 147}, {80, 147}, {160, 147}, {0, 232}, {80, 232}, {160, 232}, {0, 317}
-            , {80, 317}, {160, 317}, {240, 147}, {240, 232}, {240, 317}, {240, 402}, {320, 317}, {160, 402}};
+            , {80, 317}, {160, 317}, {240, 147}, {240, 232}, {240, 317}, {240, 402}, {320, 317}, {160, 402}, {320, 147}, {320, 232}};
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         Stage calStage = new Stage();
         calStage.setTitle("Calculator");
-        calTxtField = Components.creatingTextField("", 0, 0, 147, 400);
-        qField = Components.creatingTextField("", 0, 0, 25, 200);
+        //adding images and give sizes and (x,y)
+        Image image1 = new Image("pics/It's The Most Wonderful Time Of The Year_ Imgur Secret Santa Is Here!.jpg");
+        ImageView homeBG = new ImageView();
+        homeBG.setImage(image1);
+        homeBG.setFitWidth(609);
+        homeBG.setFitHeight(600);
+        calTxtField = Components.creatingTextField(0, 0, 147, 400);
+        memoryField = Components.creatingTextField(0, 0, 25, 200);
+        lblBG = Components.creatingLabelForBackground();
         for (int i = 0; i < calButtons.length; i++) {
             calButtons[i] = Components.creatingButton(textOnBtn[i], point[i][0], point[i][1], 85, 80);
         }
         Pane calPane = new Pane();
+        calPane.getChildren().add(homeBG);
+        calPane.getChildren().add(lblBG);
         calPane.getChildren().add(calTxtField);
-        calPane.getChildren().add(qField);
+        calPane.getChildren().add(memoryField);
         for (int i = 0; i < calButtons.length; i++) {
             calPane.getChildren().add(calButtons[i]);
         }
@@ -66,62 +79,71 @@ public class Main extends Application {
                     } else if (event.getSource() == calButtons[10]) {
                         data = Float.parseFloat(calTxtField.getText());
                         calTxtField.clear();
-                        qField.setText(data + "+");
+                        memoryField.setText(data + "+");
                         operation = 1; //Addition
                     } else if (event.getSource() == calButtons[11]) {
                         data = Float.parseFloat(calTxtField.getText());
                         calTxtField.clear();
-                        qField.setText(data + "-");
+                        memoryField.setText(data + "-");
                         operation = 2; //Subtraction
                     } else if (event.getSource() == calButtons[12]) {
                         data = Float.parseFloat(calTxtField.getText());
                         calTxtField.clear();
-                        qField.setText(data + "*");
+                        memoryField.setText(data + "*");
                         operation = 3; //Mul
                     } else if (event.getSource() == calButtons[13]) {
                         data = Float.parseFloat(calTxtField.getText());
                         calTxtField.clear();
-                        qField.setText(data + "/");
+                        memoryField.setText(data + "/");
                         operation = 4; //Div
+                    } else if (calTxtField.getText() == "" && event.getSource() == calButtons[14]) {
+                        operation = 5;
                     } else if (event.getSource() == calButtons[14]) {
                         Float secondOperand = Float.parseFloat(calTxtField.getText());
-
-
                         switch (operation) {
                             case 1: //Addition
                                 Float ans = data + secondOperand;
                                 calTxtField.setText(String.valueOf(ans));
-                                qField.setText(String.valueOf(data) + "+" + String.valueOf(secondOperand));
+                                memoryField.setText(String.valueOf(data) + "+" + String.valueOf(secondOperand));
                                 break;
                             case 2: //Subtraction
                                 ans = data - secondOperand;
                                 calTxtField.setText(String.valueOf(ans));
-                                qField.setText(String.valueOf(data) + "-" + String.valueOf(secondOperand));
+                                memoryField.setText(String.valueOf(data) + "-" + String.valueOf(secondOperand));
                                 break;
                             case 3: //Mul
                                 ans = data * secondOperand;
                                 calTxtField.setText(String.valueOf(ans));
-                                qField.setText(String.valueOf(data) + "*" + String.valueOf(secondOperand));
+                                memoryField.setText(String.valueOf(data) + "*" + String.valueOf(secondOperand));
                                 break;
                             case 4: //Div
                                 ans = 0f;
                                 try {
                                     ans = data / secondOperand;
-                                    qField.setText(String.valueOf(data) + "/" + String.valueOf(secondOperand));
+                                    memoryField.setText(String.valueOf(data) + "/" + String.valueOf(secondOperand));
                                 } catch (Exception e) {
                                     calTxtField.setText("Error");
                                 }
                                 calTxtField.setText(String.valueOf(ans));
                                 break;
+                            case 5:
+                                Components.error("ERRROR");
+                                break;
+
                         }
 
+                    } else if (event.getSource() == calButtons[16]) {
+                        calTxtField.clear();
+                    } else if (event.getSource() == calButtons[17]) {
+                        calTxtField.clear();
+                        memoryField.clear();
                     }
                 }
             });
         }
 
 
-        calStage.setScene(new Scene(calPane, 400, 609));
+        calStage.setScene(new Scene(calPane, 400, 487));
         calStage.show();
     }
 
